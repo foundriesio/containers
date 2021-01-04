cea1e4c632fa98a29f82c3ae5d02aa70644f5474
Andy Doan <andy@foundries.io>
2021-01-04 15:25:31 -0600

    k3s: Move to latest version
    This also saves kubeconfg.yaml and kubectl to a persistent location
    for the host OS to access
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- k3s/start.sh
+++ k3s/start.sh
@@ -6,6 +6,9 @@ if [ ! -f $secret ] ; then
 	cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 > $secret
 fi
 
+# Copy to persistent directory so host can run commands
+cp /bin/kubectl /var/lib/rancher/k3s/
+
 export K3S_CLUSTER_SECRET=$(cat $secret)
 
 exec k3s server --no-deploy traefik
