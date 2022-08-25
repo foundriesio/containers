d918daaa6c602bf68b21c18161a293666a551a4c
Raul Muñoz <raul@foundries.io>
2022-08-25 17:39:04 -0300

    0flutter-imx8-aarch64: Updating Flutter

--- 0flutter-imx8-aarch64/build-flutter.sh
+++ 0flutter-imx8-aarch64/build-flutter.sh
@@ -14,7 +14,7 @@ ln -sf /opt/cmake/bin/cmake /usr/local/bin/cmake
 cd /root
 git clone https://github.com/sony/flutter-elinux
 cd flutter-elinux
-git checkout 87bfe8229849f81ff8fd4cb6e7fb6c63903fd56f
+git checkout 4c1998ac7419a8da2ab7636c6f18bbbcaf601821
 mv /root/flutter-elinux /opt/
 export PATH=/opt/flutter-elinux/bin:$PATH
 echo "export PATH=$PATH" > /etc/environment
@@ -29,7 +29,7 @@ cd /root
 
 git clone https://github.com/sony/flutter-embedded-linux
 cd flutter-embedded-linux
-git checkout 047c0d1133c64f34076f5bc6f62a9919d53add61
+git checkout f1de80f92b03704d54317bb70cf94ec0c87ba24a
 mkdir /root/flutter-embedded-linux/build
 cd /root/flutter-embedded-linux/build
 
@@ -37,9 +37,4 @@ cp /opt/flutter-elinux/flutter/bin/cache/artifacts/engine/elinux-arm64-debug/lib
 cmake -DUSER_PROJECT_PATH=examples/flutter-wayland-client -DCMAKE_BUILD_TYPE=Debug ..
 cmake --build .
 
-cd /root
-git clone https://github.com/flutter/gallery 
-cd /root/gallery
-git checkout 312e0cbd271c5da57256f874ae45f87d15103890
-flutter-elinux config --enable-linux-desktop
-flutter-elinux build linux --debug
+
